4b208ab6a863bc4a7e6664b2f7d064505ad2f967
DominicSerrano <dmsrojo@gmail.com>
2018-08-10 10:43:35 -0500

    Scripts ready for remote usage. - Replaced scripts with global pm2 commands.

--- jobs/distanceHome.js
+++ jobs/distanceHome.js
@@ -9,18 +9,7 @@ const startImmediately = false;
 
 const action = async () => {
     // Fetch duration details from Google API
-    // const details = await calculateDistance({ origins: WORK_ADDRESS, destinations: HOME_ADDRESS });
-
-    const details = {
-        "origin_address": "1310 Clinton St, Nashville, TN 37203, USA",
-        "destination_address": "402 Southwood Park Pl, Nashville, TN 37217, USA",
-        "distance": "7.2 mi",
-        "duration": "13 mins",
-        "duration_in_traffic": "11 mins",
-        "status": "OK",
-        "upperLevelStatus": "OK",
-        "timestamp": "8/9/2018, 9:06:33 PM"
-    };
+    const details = await calculateDistance({ origins: WORK_ADDRESS, destinations: HOME_ADDRESS });
 
     appendResults(details);
 
